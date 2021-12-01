1bac5d2510e2d74943b5b27f7e54e135a30ca9bf
Richard Yu <vipzhicheng@gmail.com>
2021-12-01 14:36:15 +0800

    init: init logseq-plugin-block-comment

---
+++ README.md
@@ -0,0 +1,40 @@
+# Logseq Block Comment
+
+For building an automatic comment history for Logseq! You can comment any block and show the connected history on journal page, current page, block ref sidebar and "Comments" page.
+
+## Why need this
+
+If you have a long term plan, you may need to write down something about it day by day, use this way, you can keep your todo list clean and the progress connected.
+
+## Installation
+
+### Preparation
+
+* Click the 3 dots in the righthand corner and go to settings.
+* Got to advanced and enable Developer mode.
+* Restart the app.
+* Click 3 dots and go to Plugins.
+
+### Install plugins from Marketplace (recommended)
+
+* Click `Marketplace` button.
+* Install the desired plugin(s).
+
+### Install plugins manually
+
+* Download released version assets from Github.
+* Unzip it.
+* Click `Load unpacked plugin`, and select destination directory to unziped folder.
+
+## Usage
+
+* Trigger `Comment block` from block contextual menu.
+* Trigger `/comment` from block slash command.
+
+## Inspiration and Thanks
+
+The inspiration of this plugin comes from [our community](https://discord.com/channels/725182569297215569/915076465027055656/915226549857435709) by Bailey Jennings, and likely borrowed from Roam.
+
+## Licence
+
+MIT
---
+++ src/main.ts
@@ -0,0 +1,121 @@
+import '@logseq/libs';
+import { BlockEntity } from '@logseq/libs/dist/LSPlugin';
+import { format } from 'date-fns';
+
+async function getLastBlock(pageName: string): Promise<null | BlockEntity> {
+  const blocks = await logseq.Editor.getPageBlocksTree(pageName);
+  if (blocks.length === 0) {
+    return null;
+  }
+  return blocks[blocks.length - 1];
+}
+
+const handler = async (e: any) => {
+  const page = await logseq.Editor.getCurrentPage();
+  const blocks = await logseq.Editor.getCurrentPageBlocksTree();
+  console.log('blocks', blocks);
+  let findCommentBlock = blocks.find(item => item.content && item.content.startsWith('[[Comments]]'));
+
+  const block = await logseq.Editor.getBlock(e.uuid);
+  const config = await logseq.App.getUserConfigs();
+  if (!block?.properties?.id) {
+    await logseq.Editor.upsertBlockProperty(e.uuid, 'id', e.uuid);
+  }
+
+  if (page?.name) {
+    const lastBlock = await getLastBlock(page.name);
+    // Find Comment block
+    if (!findCommentBlock && lastBlock?.uuid) {
+      const newCommentBlock = await logseq.Editor.insertBlock(lastBlock.uuid, '[[Comments]]', {
+        sibling: true,
+        before: false,
+        properties: {
+          collapsed: true
+        }
+      });
+      if (newCommentBlock) {
+        findCommentBlock = newCommentBlock;
+      }
+    }
+
+    // Insert Comment blocks
+    // TODO How to check every level exist
+    if (findCommentBlock) {
+      const todayTitle = format(new Date(), config.preferredDateFormat);
+
+      // Reuse today block
+      let todayBlock, findTodayBlock: any;
+      if (findCommentBlock.children && findCommentBlock.children.length > 0) {
+        findTodayBlock = findCommentBlock.children.find((item: any) => item.content && item.content.startsWith(`[[${todayTitle}]]`));
+        if (findTodayBlock.uuid) {
+          todayBlock = findTodayBlock;
+        } else {
+          todayBlock = await logseq.Editor.insertBlock(findCommentBlock.uuid, `[[${todayTitle}]]`, {
+            sibling: false,
+            properties: {
+              collapsed: true
+            }
+          });
+        }
+      } else {
+        todayBlock = await logseq.Editor.insertBlock(findCommentBlock.uuid, `[[${todayTitle}]]`, {
+          sibling: false,
+          properties: {
+            collapsed: true
+          }
+        });
+      }
+
+      if (todayBlock?.uuid) {
+
+        // Reuse block ref block
+        let blockRefBlock, findBlockRefBlock;
+
+        if (todayBlock.children && todayBlock.children.length > 0) {
+          findBlockRefBlock = todayBlock.children.find((item: any) => item.content && item.content.startsWith(`((${e.uuid}))`));
+          if (findBlockRefBlock?.uuid) {
+            blockRefBlock = findBlockRefBlock;
+          } else {
+            blockRefBlock = await logseq.Editor.insertBlock(todayBlock?.uuid, `((${e.uuid}))`, {
+              sibling: false,
+            });
+          }
+        } else {
+          blockRefBlock = await logseq.Editor.insertBlock(todayBlock?.uuid, `((${e.uuid}))`, {
+            sibling: false,
+          });
+        }
+
+        if (blockRefBlock?.uuid) {
+          await logseq.Editor.openInRightSidebar(blockRefBlock?.uuid);
+
+          // Reuse the empty block
+          let emptyBlock;
+          if (blockRefBlock.children && blockRefBlock.children.length > 1) {
+            const lastEditingBlock = blockRefBlock.children[blockRefBlock.children.length - 1];
+            if (lastEditingBlock?.content.length === 0) {
+              emptyBlock = lastEditingBlock;
+            }
+          }
+
+          if (!emptyBlock) {
+            emptyBlock = await logseq.Editor.insertBlock(blockRefBlock?.uuid, '', {
+              sibling: false
+            });
+          }
+
+          if (emptyBlock?.uuid) {
+            await logseq.Editor.editBlock(emptyBlock?.uuid);
+          }
+        }
+      }
+    }
+  }
+
+};
+
+async function main() {
+  logseq.Editor.registerSlashCommand( `Comment block`, handler );
+  logseq.Editor.registerBlockContextMenuItem(`comment`, handler);
+}
+logseq.ready(main).catch(console.error);
