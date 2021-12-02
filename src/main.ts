488c1d57139b31847bb367b6792f7b28ac90f564
Richard Yu <vipzhicheng@gmail.com>
2021-12-02 10:27:38 +0800

    fix: comment block can not be created

--- src/main.ts
+++ src/main.ts
@@ -11,44 +11,56 @@ async function getLastBlock(pageName: string): Promise<null | BlockEntity> {
 }
 
 const handler = async (e: any) => {
-  const page = await logseq.Editor.getCurrentPage();
-  const blocks = await logseq.Editor.getCurrentPageBlocksTree();
-  console.log('blocks', blocks);
-  let findCommentBlock = blocks.find(item => item.content && item.content.startsWith('[[Comments]]'));
-
   const block = await logseq.Editor.getBlock(e.uuid);
+  if (!block || !block.uuid) {
+    return;
+  }
   const config = await logseq.App.getUserConfigs();
   if (!block?.properties?.id) {
     await logseq.Editor.upsertBlockProperty(e.uuid, 'id', e.uuid);
   }
 
+  const page = await logseq.Editor.getPage(block.page.id);
   if (page?.name) {
-    const lastBlock = await getLastBlock(page.name);
-    // Find Comment block
-    if (!findCommentBlock && lastBlock?.uuid) {
-      const newCommentBlock = await logseq.Editor.insertBlock(lastBlock.uuid, '[[Comments]]', {
-        sibling: true,
-        before: false,
-        properties: {
-          collapsed: true
+    const blocks = await logseq.Editor.getPageBlocksTree(page.name);
+    console.log('blocks', blocks);
+    let findCommentBlock = blocks.find(item => item.content && item.content.startsWith('[[Comments]]'));
+
+    if (page?.name) {
+      const lastBlock = await getLastBlock(page.name);
+      // Find Comment block
+      if (!findCommentBlock && lastBlock?.uuid) {
+        const newCommentBlock = await logseq.Editor.insertBlock(lastBlock.uuid, '[[Comments]]', {
+          sibling: true,
+          before: false,
+          properties: {
+            collapsed: true
+          }
+        });
+        if (newCommentBlock) {
+          findCommentBlock = newCommentBlock;
         }
-      });
-      if (newCommentBlock) {
-        findCommentBlock = newCommentBlock;
       }
-    }
 
-    // Insert Comment blocks
-    // TODO How to check every level exist
-    if (findCommentBlock) {
-      const todayTitle = format(new Date(), config.preferredDateFormat);
+      // Insert Comment blocks
+      // TODO How to check every level exist
+      if (findCommentBlock) {
+        const todayTitle = format(new Date(), config.preferredDateFormat);
 
-      // Reuse today block
-      let todayBlock, findTodayBlock: any;
-      if (findCommentBlock.children && findCommentBlock.children.length > 0) {
-        findTodayBlock = findCommentBlock.children.find((item: any) => item.content && item.content.startsWith(`[[${todayTitle}]]`));
-        if (findTodayBlock.uuid) {
-          todayBlock = findTodayBlock;
+        // Reuse today block
+        let todayBlock, findTodayBlock: any;
+        if (findCommentBlock.children && findCommentBlock.children.length > 0) {
+          findTodayBlock = findCommentBlock.children.find((item: any) => item.content && item.content.startsWith(`[[${todayTitle}]]`));
+          if (findTodayBlock?.uuid) {
+            todayBlock = findTodayBlock;
+          } else {
+            todayBlock = await logseq.Editor.insertBlock(findCommentBlock.uuid, `[[${todayTitle}]]`, {
+              sibling: false,
+              properties: {
+                collapsed: true
+              }
+            });
+          }
         } else {
           todayBlock = await logseq.Editor.insertBlock(findCommentBlock.uuid, `[[${todayTitle}]]`, {
             sibling: false,
@@ -57,60 +69,54 @@ const handler = async (e: any) => {
             }
           });
         }
-      } else {
-        todayBlock = await logseq.Editor.insertBlock(findCommentBlock.uuid, `[[${todayTitle}]]`, {
-          sibling: false,
-          properties: {
-            collapsed: true
-          }
-        });
-      }
 
-      if (todayBlock?.uuid) {
+        if (todayBlock?.uuid) {
 
-        // Reuse block ref block
-        let blockRefBlock, findBlockRefBlock;
+          // Reuse block ref block
+          let blockRefBlock, findBlockRefBlock;
 
-        if (todayBlock.children && todayBlock.children.length > 0) {
-          findBlockRefBlock = todayBlock.children.find((item: any) => item.content && item.content.startsWith(`((${e.uuid}))`));
-          if (findBlockRefBlock?.uuid) {
-            blockRefBlock = findBlockRefBlock;
+          if (todayBlock.children && todayBlock.children.length > 0) {
+            findBlockRefBlock = todayBlock.children.find((item: any) => item.content && item.content.startsWith(`((${e.uuid}))`));
+            if (findBlockRefBlock?.uuid) {
+              blockRefBlock = findBlockRefBlock;
+            } else {
+              blockRefBlock = await logseq.Editor.insertBlock(todayBlock?.uuid, `((${e.uuid}))`, {
+                sibling: false,
+              });
+            }
           } else {
             blockRefBlock = await logseq.Editor.insertBlock(todayBlock?.uuid, `((${e.uuid}))`, {
               sibling: false,
             });
           }
-        } else {
-          blockRefBlock = await logseq.Editor.insertBlock(todayBlock?.uuid, `((${e.uuid}))`, {
-            sibling: false,
-          });
-        }
 
-        if (blockRefBlock?.uuid) {
-          await logseq.Editor.openInRightSidebar(blockRefBlock?.uuid);
+          if (blockRefBlock?.uuid) {
+            await logseq.Editor.openInRightSidebar(blockRefBlock?.uuid);
 
-          // Reuse the empty block
-          let emptyBlock;
-          if (blockRefBlock.children && blockRefBlock.children.length > 1) {
-            const lastEditingBlock = blockRefBlock.children[blockRefBlock.children.length - 1];
-            if (lastEditingBlock?.content.length === 0) {
-              emptyBlock = lastEditingBlock;
+            // Reuse the empty block
+            let emptyBlock;
+            if (blockRefBlock.children && blockRefBlock.children.length > 0) {
+              const lastEditingBlock = blockRefBlock.children[blockRefBlock.children.length - 1];
+              if (lastEditingBlock?.content.length === 0) {
+                emptyBlock = lastEditingBlock;
+              }
             }
-          }
 
-          if (!emptyBlock) {
-            emptyBlock = await logseq.Editor.insertBlock(blockRefBlock?.uuid, '', {
-              sibling: false
-            });
-          }
+            if (!emptyBlock) {
+              emptyBlock = await logseq.Editor.insertBlock(blockRefBlock?.uuid, '', {
+                sibling: false
+              });
+            }
 
-          if (emptyBlock?.uuid) {
-            await logseq.Editor.editBlock(emptyBlock?.uuid);
+            if (emptyBlock?.uuid) {
+              await logseq.Editor.editBlock(emptyBlock?.uuid);
+            }
           }
         }
       }
     }
   }
+
 
 };
 
